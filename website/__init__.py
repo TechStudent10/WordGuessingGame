d694d6f47f1ae18a71426486312c1a199c2145ef
Tech Student <razaqhammed36@gmail.com>
2021-06-22 18:26:33 -0400

    fix init in website

--- website/__init__.py
+++ website/__init__.py
@@ -11,7 +11,7 @@ def create_app(*args, **kwargs):
         *args,
         **kwargs
     )
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     from .routes import routes
